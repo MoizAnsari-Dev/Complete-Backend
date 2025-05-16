a3bce001e67024b77c20049f2cf0ecdeaff1b492
Moiz Ansari <moizansari.india@gmail.com>
2025-05-17 02:16:59 +0530

    The Server File

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import authRouter from './routes/authRoute'
-import userRouter from './routes/userRoute'
-import subscriptionRouter from './routes/subscriptionRoute'
-
+import authRouter from './routes/authRoute.js'
+import userRouter from './routes/userRoute.js'
+import subscriptionRouter from './routes/subscriptionRoute.js'
 
 const app = express()
 dotenv.config()
 
+
+const PORT = process.env.PORT || 3000
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
 
-const PORT = process.env.PORT || 3000
 app.get("/", (req, res) => {
     res.send('Comming from Root')
 });
