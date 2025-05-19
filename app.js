d63bae5e5977c6af4824525bc9aa1668f62c8929
Moiz Ansari <moizansari.india@gmail.com>
2025-05-19 23:43:02 +0530

    Apis request limits

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import subscriptionRouter from './routes/subscriptionRoute.js'
 import connectDB from './database/mongoDB.js'
 import erorrMiddleware from './middlewares/errorMiddlewares.js'
 import cookieParser from 'cookie-parser'
+import { limiter, PORT } from './config/config.js'
 
 const app = express()
 dotenv.config()
@@ -13,16 +14,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-const PORT = process.env.PORT || 3000
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/subscriptions', subscriptionRouter)
-
 app.use(erorrMiddleware)
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', limiter, userRouter)
+app.use('/api/v1/subscriptions', limiter, subscriptionRouter)
 
-app.get("/", (req, res) => {
-    res.send('Comming from Root')
-});
+
 
 app.listen(PORT, async () => {
     await connectDB()
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { login, logout, signUp } from "../controller/authController.js";
+import { limiter } from "../config/config.js";
 
 const authRouter = Router()
 
 authRouter.post("/sign-up", signUp)
 
-authRouter.post("/login", login)
+authRouter.post("/login", limiter, login)
 
 authRouter.post("/logout", logout)
 
