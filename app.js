69c0184ae10901d7e305ca49a153e31689e34e59
Moiz Ansari <moizansari.india@gmail.com>
2025-05-25 15:59:16 +0530

    installing the cors

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ import connectDB from './database/mongoDB.js'
 import erorrMiddleware from './middlewares/errorMiddlewares.js'
 import cookieParser from 'cookie-parser'
 import { limiter, PORT } from './config/config.js'
+import cors from 'cors'
 
-const app = express()
-dotenv.config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+const app = express()// Create an Express application
+dotenv.config()// Load environment variables from .env file
+app.use(cors())// Allow CORS for all origins 
+app.use(express.json())// Parse JSON bodies
+app.use(express.urlencoded({ extended: false }))// Parse URL-encoded bodies
+app.use(cookieParser())// Parse cookies
 
-app.use(erorrMiddleware)
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users', limiter, userRouter)
-app.use('/api/v1/subscriptions', limiter, subscriptionRouter)
+app.use(erorrMiddleware)// Use error handling middleware
+app.use('/api/v1/auth', authRouter)// Define routes for authentication
+app.use('/api/v1/users', limiter, userRouter)// Define routes for user management
+app.use('/api/v1/subscriptions', limiter, subscriptionRouter)// Define routes for subscription management
 
 app.listen(PORT, async () => {
     await connectDB()
